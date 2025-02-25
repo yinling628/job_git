c3ec558640b9abb0bc5ded8a5b4f9a8b849b7349
yinling <yinling@udbs.cn>
2025-02-25 17:22:21 +0800

    version 1.0

---
+++ app.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# 创建一个简单的数据集
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([1, 3, 2, 3, 5])
+
+# 创建线性回归模型
+model = LinearRegression()
+model.fit(X, y)
+
+# 进行预测
+predictions = model.predict([[6]])
+print("Prediction for input 6:", predictions)
+
